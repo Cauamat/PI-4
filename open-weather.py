2ea80f73d3eee071f620e6ade753fdfa41b7e4a7
cauamaturana <cauamaturana@gmail.com>
2025-09-17 05:13:14 -0300

    Codigo teste para dados de SP

--- open-weather.py
+++ open-weather.py
@@ -1,1 +1,14 @@
 import requests
+
+with open("api_key.txt", "r") as file:
+    api_key = file.read().strip()
+
+lat = -23.533773 # Latitude de São Paulo
+lon = -46.625290 # Longitude de São Paulo
+
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+
+response = requests.get(url)
+response.raise_for_status()
+data = response.json()
+print(data)
